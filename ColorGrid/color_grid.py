6465f94af2c9a2ce3bcbba8704221c858884c3c5
João Otávio Biondo <joao.o.b@hotmail.com>
2017-06-06 13:19:02 -0300

    Refatorando código para ficar mais genérico

--- ColorGrid/color_grid.py
+++ ColorGrid/color_grid.py
@@ -1,55 +1,40 @@
 import numpy as np
 import cv2
 
-#c1 = color top left
-#c2 = color top right
-#c3 = color bottom left
-#c4 = color bottom right 
-def makeColorGrid(c1, c2, c3, c4, gridSize, window_width=500, window_height=650):
-	TILE_WIDTH = window_width / gridSize
-	TILE_HEIGHT = window_height / gridSize
+# c1 = top left color. Format (R, G, B)
+# c2 = top right color. Format (R, G, B)
+# c3 = bottom left color. Format (R, G, B)
+# c4 = bottom right color. Format (R, G, B)
+def makeColorGrid(c1, c2, c3, c4, n_lines, n_columns, window_width=500, window_height=650):
+	TILE_WIDTH = window_width / n_columns
+	TILE_HEIGHT = window_height / n_lines
 
-	colors = np.zeros((gridSize, gridSize, 3))
+	colors = np.zeros((n_lines, n_columns, 3))
 	
-	# TOP
-	colors[0, 0:gridSize, 2] = np.linspace(c1[0], c2[0], gridSize) # RED
-	colors[0, 0:gridSize, 1] = np.linspace(c1[1], c2[1], gridSize) # GREEN
-	colors[0, 0:gridSize, 0] = np.linspace(c1[2], c2[2], gridSize) # BLUE
-
 	# LEFT
-	colors[0:gridSize, 0, 2] = np.linspace(c1[0], c3[0], gridSize) # RED
-	colors[0:gridSize, 0, 1] = np.linspace(c1[1], c3[1], gridSize) # GREEN
-	colors[0:gridSize, 0, 0] = np.linspace(c1[2], c3[2], gridSize) # BLUE
-
-	# BOTTOM
-	colors[gridSize - 1, 0:gridSize, 2] = np.linspace(c3[0], c4[0], gridSize) # RED
-	colors[gridSize - 1, 0:gridSize, 1] = np.linspace(c3[1], c4[1], gridSize) # GREEN
-	colors[gridSize - 1, 0:gridSize, 0] = np.linspace(c3[2], c4[2], gridSize) # BLUE
+	colors[:, 0, 2] = np.linspace(c1[0], c3[0], n_lines) # RED
+	colors[:, 0, 1] = np.linspace(c1[1], c3[1], n_lines) # GREEN
+	colors[:, 0, 0] = np.linspace(c1[2], c3[2], n_lines) # BLUE
 
 	# RIGHT
-	colors[0:gridSize, gridSize - 1, 2] = np.linspace(c2[0], c4[0], gridSize) # RED
-	colors[0:gridSize, gridSize - 1, 1] = np.linspace(c2[1], c4[1], gridSize) # GREEN
-	colors[0:gridSize, gridSize - 1, 0] = np.linspace(c2[2], c4[2], gridSize) # BLUE
+	colors[:, -1, 2] = np.linspace(c2[0], c4[0], n_lines) # RED
+	colors[:, -1, 1] = np.linspace(c2[1], c4[1], n_lines) # GREEN
+	colors[:, -1, 0] = np.linspace(c2[2], c4[2], n_lines) # BLUE
 
-	for i in range(1, gridSize - 1):
+	for i in range(n_lines):
 		color1 = colors[i, 0]
-		color2 = colors[i, gridSize - 1]
+		color2 = colors[i, -1]
 		
-		colors[i, 0:gridSize, 2] = np.linspace(color1[2], color2[2], gridSize) # RED
-		colors[i, 0:gridSize, 1] = np.linspace(color1[1], color2[1], gridSize) # GREEN
-		colors[i, 0:gridSize, 0] = np.linspace(color1[0], color2[0], gridSize) # BLUE
+		colors[i, :, 2] = np.linspace(color1[2], color2[2], n_columns) # RED
+		colors[i, :, 1] = np.linspace(color1[1], color2[1], n_columns) # GREEN
+		colors[i, :, 0] = np.linspace(color1[0], color2[0], n_columns) # BLUE
 
 
-	#255 -   -   0
-	#-   -   -   0
-	#-   -   -   0
-	#0   0   0   0
-
-	
+	# OpenCV uses the 0 to 1 range for RGB color
 	colors = colors / 255.0
 
+	# Creates the image with the colors
 	image = colors.repeat(TILE_HEIGHT, axis=0).repeat(TILE_WIDTH, axis=1)
-
 
 	cv2.imshow('Color grid', image)
 	cv2.waitKey(0)
@@ -58,7 +43,7 @@ def makeColorGrid(c1, c2, c3, c4, gridSize, window_width=500, window_height=650)
 
 # MAIN
 if __name__ == '__main__':
-	#makeColorGrid([0,0,0], [255,255,255],[0,255,0],[0,0,255], 70)
-	makeColorGrid([232, 149, 139], [48, 113, 181],[218, 239, 125],[0, 205, 217], 5)
-	makeColorGrid([232, 149, 139], [48, 113, 181],[218, 239, 125],[0, 205, 217], 50)
+	#makeColorGrid((0,0,0), (255,255,255),(0,255,0),(0,0,255), 70)
+	makeColorGrid((232, 149, 139), (48, 113, 181), (218, 239, 125), (0, 205, 217), 5, 5)
+	makeColorGrid((232, 149, 139), (48, 113, 181), (218, 239, 125), (0, 205, 217), 100, 100)
 
