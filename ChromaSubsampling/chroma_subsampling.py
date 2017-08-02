57e04ddd6c1c81c99d051303c5bae0bd7d24bce5
João Otávio Biondo <joao.o.b@hotmail.com>
2017-08-02 11:36:34 -0300

    Modificado color grid e FEITO Chroma Subsampling

--- ChromaSubsampling/chroma_subsampling.py
+++ ChromaSubsampling/chroma_subsampling.py
@@ -1,26 +1,81 @@
 import numpy as np
 import cv2
 
-def chroma_subsampling(image, n):
+
+# Retorna as 3 bandas em uma tupla da forma (Y, Cr, Cb)
+def chroma_subsampling(image, type):
     ycrcb = cv2.cvtColor(image, cv2.COLOR_BGR2YCR_CB)
+
+    Y = ycrcb[:, :, 0]
+    Cr = ycrcb[:, :, 1]
+    Cb = ycrcb[:, :, 2]
+
+    if (type == "4:2:2"):
+        return (Y, h_downsample(Cr, 2), h_downsample(Cb, 2))
+    elif (type == "4:1:1"):
+        return (Y, h_downsample(Cr, 4), h_downsample(Cb, 4))
+    elif (type == "4:4:0"):
+        return (Y, v_downsample(Cr, 2), v_downsample(Cb, 2))
+    elif (type == "4:2:0"):
+        return (Y, h_downsample(v_downsample(Cr, 2), 2), h_downsample(v_downsample(Cb, 2), 2))
+    elif (type == "4:1:0"):
+        return (Y, h_downsample(v_downsample(Cr, 2), 4), h_downsample(v_downsample(Cb, 2), 4))
+    elif (type == "4:2:1"):
+        return (Y, h_downsample(Cr, 2), h_downsample(Cb, 4))
     
-    ycrcb[:,:,1] = horizontal_upsample(horizontal_downsample(ycrcb[:,:,1], n), n)
-    ycrcb[:,:,2] = horizontal_upsample(horizontal_downsample(ycrcb[:,:,2], n), n)
+    return None
 
-    return cv2.cvtColor(ycrcb, cv2.COLOR_YCR_CB2BGR)
+
+def upsampling(channels, type):
+    Y, Cr, Cb = channels
+    if (type == "4:2:2"):
+        return (Y, h_upsample(Cr, 2), h_upsample(Cb, 2))
+    elif (type == "4:1:1"):
+        return (Y, h_upsample(Cr, 4), h_upsample(Cb, 4))
+    elif (type == "4:4:0"):
+        return (Y, v_upsample(Cr, 2), v_upsample(Cb, 2))
+    elif (type == "4:2:0"):
+        return (Y, h_upsample(v_upsample(Cr, 2), 2), h_upsample(v_upsample(Cb, 2), 2))
+    elif (type == "4:1:0"):
+        return (Y, h_upsample(v_upsample(Cr, 2), 4), h_upsample(v_upsample(Cb, 2), 4))
+    elif (type == "4:2:1"):
+        return (Y, h_upsample(Cr, 2), h_upsample(Cb, 4))
+
+
+def h_downsample(image_channel, n):
+    return image_channel[:, ::n]
+
+def v_downsample(image_channel, n):
+    return image_channel[::n, :]
+
+def h_upsample(image_channel, n):
+    return np.repeat(image_channel, n, axis=1) 
+
+def v_upsample(image_channel, n):
+    return np.repeat(image_channel, n, axis=0)
+
+
+
+def show_images(channels, type):
+    Y, Cr, Cb = channels
+
+    lin, col = Y.shape
+    channels_img = np.zeros((lin, col + Cr.shape[1] + Cb.shape[1]), np.uint8)
+    channels_img[:, :col] = Y
+    channels_img[:Cr.shape[0], col:col+Cr.shape[1]] = Cr
+    channels_img[:Cb.shape[0], col+Cr.shape[1]:] = Cb
     
 
-def downsample(image, n):
-    return image[::n, ::n]
+    cv2.imshow('YCrCb subsampled channels - ' + type, channels_img)
 
-def horizontal_downsample(image, n):
-    return image[:, ::n]
+    ups_channels = upsampling(channels, type)
 
-def upsample(image, n):
-    return np.repeat(np.repeat(image, n, axis=0), n, axis=1)  
+    img = np.dstack(ups_channels)
+    img = cv2.cvtColor(img, cv2.COLOR_YCR_CB2BGR)
 
-def horizontal_upsample(image, n):
-    return np.repeat(image, n, axis=1) 
+    cv2.imshow('Chroma subsampling - ' + type, img)
+
+    #cv2.imwrite("chromasample.png", img)
 
 
 
@@ -29,13 +84,25 @@ if __name__ == '__main__':
 
     image = cv2.imread('lenna.png', cv2.IMREAD_COLOR)
 
-    img = chroma_subsampling(image, 16)
+    # Tipos disponíveis:
+    # "4:2:2"
+    # "4:1:1"
+    # "4:4:0"
+    # "4:2:0"
+    # "4:1:0"
+    # "4:2:1"
+    type = input("Digite o tipo do chroma subsampling (J:a:b) > ")
 
-    #cv2.imwrite("chromasample.png", img)
-    cv2.imshow('Original', image)
-    cv2.imshow('Chroma subsampling', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    channels = chroma_subsampling(image, type)
+
+    if (channels != None):
+        cv2.imshow('Original', image)
+        show_images(channels, type)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("Tipo não suportado.")
     
 
 
--- ColorGrid/ColorGrid_Reshape&Linspace.py
+++ ColorGrid/ColorGrid_Reshape&Linspace.py
@@ -1,35 +1,31 @@
 import numpy as np
 import cv2
-import time
 
-# tpc = top left color. Format (R, G, B)
+# returns a gradient from color 'c1' to color 'c2' with 'size' lines
+def getGradient(c1, c2, size):
+    a = np.empty((size, 3))
+    a[:, 2] = np.linspace(c1[0], c2[0], size) # Red
+    a[:, 1] = np.linspace(c1[1], c2[1], size) # Green
+    a[:, 0] = np.linspace(c1[2], c2[2], size) # Blue
+    return a
+
+# tlc = top left color. Format (R, G, B)
 # trc = top right color. Format (R, G, B)
 # blc = bottom left color. Format (R, G, B)
 # brc = bottom right color. Format (R, G, B)
-def makeColorGrid(tpc, trc, blc, brc, n_lines, n_columns, window_width=500, window_height=650):
-    start_time = time.time()
+def makeColorGrid(tlc, trc, blc, brc, n_lines, n_columns, window_width=500, window_height=650):
     TILE_WIDTH = window_width / n_columns
     TILE_HEIGHT = window_height / n_lines
     
     colors = np.empty((n_lines, n_columns, 3))
-    c = np.linspace(0, 1, n_columns)
+
+    weights = np.linspace(0, 1, n_columns)
+
+    left = getGradient(tlc, blc, n_lines).reshape(-1, 1) # First column (left)
+    right = getGradient(trc, brc, n_lines).reshape(-1, 1) # Last column (right)
     
-    # Red
-    colors[:, 0, 2] = np.linspace(tpc[0], blc[0], n_lines)
-    colors[:, -1, 2] = np.linspace(trc[0], brc[0], n_lines)
+    r = left + (right - left) * weights
 
-    # Green
-    colors[:, 0, 1] = np.linspace(tpc[1], blc[1], n_lines)
-    colors[:, -1, 1] = np.linspace(trc[1], brc[1], n_lines)
-
-    # Blue
-    colors[:, 0, 0] = np.linspace(tpc[2], blc[2], n_lines) 
-    colors[:, -1, 0] = np.linspace(trc[2], brc[2], n_lines)
-
-    a = colors[:, 0, :].reshape(-1, 1) # First column
-    b = colors[:, -1, :].reshape(-1, 1) # Last column
-    
-    r = a + (b - a) * c
     colors[:, :, 0] = r[0::3]
     colors[:, :, 1] = r[1::3]
     colors[:, :, 2] = r[2::3]
@@ -39,8 +35,6 @@ def makeColorGrid(tpc, trc, blc, brc, n_lines, n_columns, window_width=500, wind
     # Creates the image with the colors
     image = colors.repeat(TILE_HEIGHT, axis=0).repeat(TILE_WIDTH, axis=1)
 
-    print((time.time() - start_time)*1000)
-
     cv2.imshow('Color grid', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -48,7 +42,7 @@ def makeColorGrid(tpc, trc, blc, brc, n_lines, n_columns, window_width=500, wind
 
 # MAIN
 if __name__ == '__main__':
-    makeColorGrid((0,0,0), (240,240,240),(120,120,120),(0,0,0), 5, 10)
-    #makeColorGrid((232, 149, 139), (48, 113, 181), (218, 239, 125), (0, 205, 217), 5, 5)
+    #makeColorGrid((0,0,0), (240,240,240),(120,120,120),(0,0,0), 5, 10)
+    makeColorGrid((232, 149, 139), (48, 113, 181), (218, 239, 125), (0, 205, 217), 5, 5)
     #makeColorGrid((232, 149, 139), (48, 113, 181), (218, 239, 125), (0, 205, 217), 100, 100)
 
